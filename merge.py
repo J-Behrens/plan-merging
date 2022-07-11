6878c00894b3133f1bf8f230972288f5d8643547
Jan Philipp Behrens <80356280+J-Behrens@users.noreply.github.com>
2022-07-11 13:25:58 +0200

    Update merge.py

--- merge.py
+++ merge.py
@@ -1,103 +1,106 @@
-import argparse, os, re, subprocess
+import argparse, os, re, subprocess, random, shutil
 from subprocess import getoutput
+
+def write_instance(dest):
+	with open(args.instance, "r") as inst:
+		with open(dest, "w") as fu:
+			fu.write(inst.read())	
 
 def plan_exists():
 	present = True
-	instance_file = open(INSTANCE,'r')
+	instance_file = open(args.instance,'r')
 	if (re.search('%plan', instance_file.read())) is None:
 		present = False
 	instance_file.close()
 	return(present)
-
-def wait():
-	output = getoutput('clingo ' + INSTANCE + ' ' + args.encoding + ' -V0 --out-atomf=%s. | head -n -1')
-	with open('temp.lp', "w") as temp:
-		temp.write(output)
-	print('Vertex conflict resolved!')
-	while (re.search('vc', getoutput('clingo temp.lp encoding/test_vc.lp -V0 --out-atomf=%s. | head -n -1'))) is not None:
-		output = getoutput('clingo temp.lp ' + args.encoding + ' -V0 --out-atomf=%s. | head -n -1')
-		with open('temp.lp', "w") as temp:
-			temp.write(output)
-		print('Vertex conflict resolved!')
-	
-def edge():
-	output = getoutput('clingo ' + INSTANCE + ' encoding/edge.lp -V0 --out-atomf=%s. | head -n -1')
-	with open('temp.lp', "w") as temp:
-		temp.write(output)
-	print('Edge conflict resolved!')
-	while (re.search('ec', getoutput('clingo temp.lp encoding/test_ec.lp -V0 --out-atomf=%s. | head -n -1'))) is not None:
-		output = getoutput('clingo temp.lp encoding/edge.lp -V0 --out-atomf=%s. | head -n -1')
-		with open('temp.lp', "w") as temp:
-			temp.write(output)
-		print('Edge conflict resolved!')
-
-def merge_alternating():
-	with open('temp.lp', "w") as temp:
-		temp.write('')
-	if (re.search('vc', getoutput('clingo ' + INSTANCE + ' temp.lp encoding/test_vc.lp -V0 --out-atomf=%s. | head -n -1'))) is not None:
-		output = getoutput('clingo ' + INSTANCE + ' ' + args.encoding + ' -V0 --out-atomf=%s. | head -n -1')
-		with open('temp.lp', "w") as temp:
-			temp.write(output)
-	elif (re.search('ec', getoutput('clingo ' + INSTANCE + ' temp.lp encoding/test_ec.lp -V0 --out-atomf=%s. | head -n -1'))) is not None:
-		print('Edge conflict(s) found!')
-		output = getoutput('clingo ' + INSTANCE + ' encoding/edge.lp -V0 --out-atomf=%s. | head -n -1')
-		with open('temp.lp', "w") as temp:
-			temp.write(output)
-	while (re.search('vc', getoutput('clingo temp.lp encoding/test_vc.lp -V0 --out-atomf=%s. | head -n -1'))) is not None or (re.search('ec', getoutput('clingo temp.lp encoding/test_ec.lp -V0 --out-atomf=%s. | head -n -1'))) is not None:
-		if (re.search('vc', getoutput('clingo temp.lp encoding/test_vc.lp -V0 --out-atomf=%s. | head -n -1'))) is not None:
-			output = getoutput('clingo temp.lp ' + args.encoding + ' -V0 --out-atomf=%s. | head -n -1')
+			
+def alt_fixed(robot_id):
+	if v_conflict():
+		output = getoutput('clingo temp.lp encoding/vertex_fixed.lp -V0 --out-atomf=%s. -c fixed=' + str(robot_id) + ' | head -n -1')
+		if error(output):
+			print(output)
+			return(True)
+		else:
 			with open('temp.lp', "w") as temp:
 				temp.write(output)
-			print('Vertex conflict resolved!')
-		if (re.search('ec', getoutput('clingo temp.lp encoding/test_ec.lp -V0 --out-atomf=%s. | head -n -1'))) is not None:
-			output = getoutput('clingo temp.lp encoding/edge.lp -V0 --out-atomf=%s. | head -n -1')
+	if e_conflict():
+		output = getoutput('clingo temp.lp encoding/edge_fixed.lp -V0 --out-atomf=%s. -c fixed=' + str(robot_id) + ' | head -n -1')
+		if error(output):
+			print(output)
+			return(True)
+		else:
 			with open('temp.lp', "w") as temp:
 				temp.write(output)
-			print('Edge conflict resolved!')
 
-def merge_vc_first():
-	with open('temp.lp', "w") as temp:
-		temp.write('')
-	if (re.search('vc', getoutput('clingo ' + INSTANCE + ' temp.lp encoding/test_vc.lp -V0 --out-atomf=%s. | head -n -1'))) is not None:
-		print('Vertex conflict(s) found!')
-		wait()
-	elif (re.search('ec', getoutput('clingo ' + INSTANCE + ' temp.lp encoding/test_ec.lp -V0 --out-atomf=%s. | head -n -1'))) is not None:
-		print('Edge conflict(s) found!')
-		edge()	
-	while (re.search('vc', getoutput('clingo temp.lp encoding/test_vc.lp -V0 --out-atomf=%s. | head -n -1'))) is not None or (re.search('ec', getoutput('clingo temp.lp encoding/test_ec.lp -V0 --out-atomf=%s. | head -n -1'))) is not None:
-		if (re.search('vc', getoutput('clingo temp.lp encoding/test_vc.lp -V0 --out-atomf=%s. | head -n -1'))) is not None:
-			print('Vertex conflict(s) found!')
-			wait()
-		if (re.search('ec', getoutput('clingo temp.lp encoding/test_ec.lp -V0 --out-atomf=%s. | head -n -1'))) is not None:
-			print('Edge conflict(s) found!')
-			edge()
-	print('All conflicts resolved!')
-
-def merge_ec_first():
-	with open('temp.lp', "w") as temp:
-		temp.write('')
-	if (re.search('ec', getoutput('clingo ' + INSTANCE + ' encoding/test_ec.lp -V0 --out-atomf=%s. | head -n -1'))) is not None:
-		print('Edge conflict(s) found!')
-		edge()
-	elif (re.search('vc', getoutput('clingo ' + INSTANCE + ' encoding/test_vc.lp -V0 --out-atomf=%s. | head -n -1'))) is not None:
-		print('Vertex conflict(s) found!')
-		wait()	
-	while (re.search('ec', getoutput('clingo temp.lp encoding/test_ec.lp -V0 --out-atomf=%s. | head -n -1'))) is not None or (re.search('vc', getoutput('clingo temp.lp encoding/test_vc.lp -V0 --out-atomf=%s. | head -n -1'))) is not None:
-		if (re.search('ec', getoutput('clingo temp.lp encoding/test_ec.lp -V0 --out-atomf=%s. | head -n -1'))) is not None:
-			print('Edge conflict(s) found!')
-			edge()
-		if (re.search('vc', getoutput('clingo temp.lp encoding/test_vc.lp -V0 --out-atomf=%s. | head -n -1'))) is not None:
-			print('Vertex conflict(s) found!')
-			wait()
-	print('All conflicts resolved!')
-
+def error(output):
+	if (re.search('ERROR', output)) is None and output != '': return(False)
+	else: return(True)
+		
+def v_conflict():
+	if (re.search('vc', getoutput('clingo temp.lp encoding/test_vc.lp -V0 | head -n -1'))) is None: return(False)
+	else: return(True)	
+		
+def e_conflict():
+	if (re.search('ec', getoutput('clingo temp.lp encoding/test_ec.lp -V0 | head -n -1'))) is None: return(False)
+	else: return(True)	
+		
+def conflict():
+	if v_conflict() or e_conflict(): return(True)
+	else: return(False)
+		
+def prio():
+	num_robots = getoutput('clingo encoding/numR.lp ' + args.instance + ' -V0| head -n -1')
+	num_robots = int((num_robots.split('('))[1].split(')')[0])
+	order = list(range(1, num_robots+1))
+	stop = False
+	retry = True
+	while retry:
+		with open('temp.lp', "w") as temp:
+			temp.write('')
+		random.shuffle(order)
+		print(order)
+		
+		for robot_id in order:
+			init = getoutput('clingo encoding/get_init.lp -c id=' + str(robot_id) + ' ' + args.instance + ' -V0 --out-atomf=%s. | head -n -1')
+			with open('temp.lp', "a") as temp:
+				temp.write(init)
+			path = getoutput('clingo encoding/get_path.lp -c id=' + str(robot_id) + ' ' + args.instance + ' -V0 --out-atomf=%s. | head -n -1')
+			with open('temp.lp', "a") as temp:
+				temp.write(path)
+			if robot_id != order[0]:
+				count = 0
+				count_rep = 0
+				while conflict():
+					count = count + 1
+					shutil.copyfile('temp.lp', 'temp-1.lp')
+					if alt_fixed(robot_id):
+						stop = True
+						break
+					v_cs        = getoutput('clingo temp.lp   encoding/test_vc_fixed.lp -V0 -c fixed=' + str(robot_id) + '| head -n -1')
+					v_cs_before = getoutput('clingo temp-1.lp encoding/test_vc_fixed.lp -V0 -c fixed=' + str(robot_id) + '| head -n -1')
+					e_cs        = getoutput('clingo temp.lp   encoding/test_ec_fixed.lp -V0 -c fixed=' + str(robot_id) + '| head -n -1')
+					e_cs_before = getoutput('clingo temp-1.lp encoding/test_ec_fixed.lp -V0 -c fixed=' + str(robot_id) + '| head -n -1')
+					
+					if v_cs_before in v_cs and e_cs==e_cs_before:
+						count_rep = count_rep + 1
+						if count_rep==2: break
+					if count == 20: break
+				if count_rep==2: break
+				if count == 20:
+					retry = True
+					break
+			if stop:
+				retry = False
+				break
+		if count != 20 and count_rep!=2:
+			retry = False
 
 def single():
 	if plan_exists(): print("Instance already contains single agent plans!")
 	horizon=0
 	while not plan_exists():
 		print('Try with horizon = ' + str(horizon))
-		output = getoutput('clingo encoding/single_agent.lp -c horizon=' + str(horizon) + ' ' + INSTANCE + ' -V0 --out-atomf=%s. --out-ifs="\n" | head -n -1')
+		output = getoutput('clingo encoding/single_agent.lp -c horizon=' + str(horizon) + ' ' + args.instance + ' -V0 --out-atomf=%s. --out-ifs="\n" | head -n -1')
 		if output != '':
 			with open(args.instance, "a") as instance:
 				instance.write("\n%plan\n" + output)
@@ -108,18 +111,12 @@ def single():
 parser = argparse.ArgumentParser()
 
 parser.add_argument("-i", "--instance",	help="Instance file",					required=True)
-parser.add_argument("-e", "--encoding",	help="Merging encoding",				type=str)
-parser.add_argument("-s", "--single",		help="Get single agent plans",  		  	action='store_true')
+parser.add_argument("-s", "--single",		help="Get single agent plans and add to instance",  	action='store_true')
 parser.add_argument("-v", "--visualize",	help="Visualize output with asprilo visualizer",    	action='store_true')
 #parser.add_argument("-b", "--benchmark",	help="Analyze runtimes",       			action='store_true')
 
 args = parser.parse_args()
 
-INSTANCE = args.instance
-
 if args.single: single()
-if args.encoding is not None:
-	merge_alternating() # or
-	#merge_ec_first()   # or
-	#merge_vc_first()
+else: prio()
 if args.visualize: os.system('viz -p temp.lp')
